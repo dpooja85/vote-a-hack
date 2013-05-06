d87e244ba9cc7d05b0379628fc35ba8a32342830
Dave Cecere <dave.cecere@jumptap.com>
2013-05-06 17:37:27 -0400

    delete old project

--- src/main/java/com/cecere/springdemo/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cecere.springdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cecere.springdemo.domain.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
--- src/test/client/curlCreate.sh
+++
@@ -1,2 +0,0 @@
-curl -v -X POST -d @./content/demoObjectNoId.json -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:8080/SpringRest/demo
-
